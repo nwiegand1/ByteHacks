792784f0e1ffb14065d15204f81842c5daaf4486
Summer Wu <summer.wu82@gmail.com>
2017-06-24 23:41:16 -0400

    server 2

---
+++ app.js
@@ -0,0 +1,39 @@
+'use strict'
+const http = require('http')
+const fs = require('fs');
+const Bot = require('messenger-bot')
+
+let bot = new Bot({
+  token: 'EAAZAsRtD1e0UBAM55DeZA4oq1pDx0tQj84vtqrC803ZA1ixEP1bdA8ErdrmndrllXZB1OoqF6IRZAkFYQf1EMOZBBsFNUGtvELWbdNkA1XgsVTlXet90p2mjypH7P9BG0IwBU9UzsVnIZBKa8RUVhpaESgR205dd82rZC2u3ZCgu9IAZDZD',
+  verify: 'VERIFY_TOKEN'
+})
+
+bot.on('error', (err) => {
+  console.log(err.message)
+})
+
+bot.on('message', (payload, reply) => {
+  let text = payload.message.text
+
+  bot.getProfile(payload.sender.id, (err, profile) => {
+    if (err) throw err
+
+    reply({ text }, (err) => {
+      if (err) throw err
+
+      console.log(`Echoed back to ${profile.first_name} ${profile.last_name}: ${text}`)
+    })
+  })
+})
+
+// bot server
+http.createServer(bot.middleware()).listen(8445)
+
+// map server
+http.createServer(function (req, res) {
+  fs.readFile('fb.html',function (err, data){
+    res.writeHead(200, {'Content-Type': 'text/html','Content-Length':data.length});
+    res.write(data);
+    res.end();
+  });
+}).listen(process.env.PORT || 5000)
