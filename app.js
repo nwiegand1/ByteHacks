daa5cca3b5e67d8781bd31d9f862b7f3719ce1ed
Summer Wu <summer.wu82@gmail.com>
2017-06-25 10:40:35 -0400

    reply with id

--- app.js
+++ app.js
@@ -34,7 +34,7 @@ bot.on('message', (payload, reply) => {
   bot.getProfile(payload.sender.id, (err, profile) => {
     if (err) throw err
 
-    reply({ text }, (err) => {
+    reply({ payload.sender.id }, (err) => {
       if (err) throw err
 
       console.log(`Echoed back to ${profile.first_name} ${profile.last_name}: ${text}`)
