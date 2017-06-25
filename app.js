8f865bce7e3445f4ae94e614f6b1911a68a58d8f
Summer Wu <summer.wu82@gmail.com>
2017-06-25 10:54:33 -0400

    log id

--- app.js
+++ app.js
@@ -56,12 +56,12 @@ bot.on('postback', (payload, reply, actions) => {
 // receives all other text
 bot.on('message', (payload, reply) => {
   let text = payload.message.text
-  let sender_id = String(payload.sender.id)
+  console.log(payload.sender.id)
 
   bot.getProfile(payload.sender.id, (err, profile) => {
     if (err) throw err
 
-    reply({ sender_id }, (err) => {
+    reply({ text }, (err) => {
       if (err) throw err
 
       console.log(`Echoed back to ${profile.first_name} ${profile.last_name}: ${text}`)
