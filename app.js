6562c2d3fdd3d6fa838830b157a227ec77dc95d2
Summer Wu <summer.wu82@gmail.com>
2017-06-25 10:34:30 -0400

    firebase

--- app.js
+++ app.js
@@ -3,6 +3,9 @@ const http = require('http')
 const fs = require('fs');
 const Bot = require('messenger-bot')
 
+var firebase = require('firebase');
+var app = firebase.initializeApp({ ... });
+
 let bot = new Bot({
   token: 'EAAZAsRtD1e0UBAM55DeZA4oq1pDx0tQj84vtqrC803ZA1ixEP1bdA8ErdrmndrllXZB1OoqF6IRZAkFYQf1EMOZBBsFNUGtvELWbdNkA1XgsVTlXet90p2mjypH7P9BG0IwBU9UzsVnIZBKa8RUVhpaESgR205dd82rZC2u3ZCgu9IAZDZD',
   verify: 'VERIFY_TOKEN'
